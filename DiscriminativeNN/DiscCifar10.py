20ce3961778f2d5176ce73c1a36f18829bf091e7
lucas-swiniarski <lucas.swiniarski@gmail.com>
2017-03-10 13:43:19 -0500

    Training on Cifar10

---
+++ DiscriminativeNN/DiscCifar10.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch
+
+class _netD(nn.Module):
+    def __init__(self, ngpu, nz, ndf, nc):
+        super(_netD, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+    def forward(self, input):
+        gpu_ids = None
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            gpu_ids = range(self.ngpu)
+        output = nn.parallel.data_parallel(self.main, input, gpu_ids)
+        return output.view(-1, 1)
---
+++ QualityAssessor/main.py
@@ -0,0 +1,177 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='cifar10',help='cifar10 | lsun | imagenet | folder | lfw ')
+parser.add_argument('--dataroot', default='../data', type=str, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
+parser.add_argument('--ngpu'  , type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--outf', default='../TrainedNetworks', help='folder to output images and model checkpoints')
+parser.add_argument('--name', default='dcgan', help='Name of the saved modle')
+parser.add_argument('--wgan', type=bool, default=False, help='Training a Wasserstein Gan or a DC-GAN')
+parser.add_argument('--clamp', type=float, default=0.01, help='Clamping parameter of the W-Gan')
+parser.add_argument('--n-critic', type=float, default=5, help='Times training the discriminator vs generator')
+
+args = parser.parse_args()
+
+args.manualSeed = random.randint(1, 10000) # fix seed
+print("Random Seed: ", args.manualSeed)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+
+###
+# Import Generative and Disriminative Networks.
+###
+
+sys.path.append("../DiscriminativeNN")
+sys.path.append("../GenerativeNN")
+
+if args.dataset == 'cifar10':
+    import GenCifar10 as ModelG
+    import DiscCifar10 as ModelD
+
+print(args)
+
+if args.dataset == 'cifar10':
+    dataset = dset.CIFAR10(root=args.dataroot, download=True,
+                           transform=transforms.Compose([
+                               transforms.Scale(args.imageSize),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ])
+    )
+
+assert dataset
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batchSize,
+                                         shuffle=True, num_workers=int(args.workers))
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+ngpu = int(args.ngpu)
+nz = int(args.nz)
+ngf = int(args.ngf)
+ndf = int(args.ndf)
+nc = 3
+
+netG = ModelG._netG(ngpu, nz, ngf, nc)
+netG.apply(weights_init)
+if args.netG != '':
+    netG.load_state_dict(torch.load(args.netG))
+print(netG)
+
+netD = ModelD._netD(ngpu, nz, ndf, nc)
+netD.apply(weights_init)
+if args.netD != '':
+    netD.load_state_dict(torch.load(args.netD))
+print(netD)
+
+criterion = nn.BCELoss()
+
+input = torch.FloatTensor(args.batchSize, 3, args.imageSize, args.imageSize)
+noise = torch.FloatTensor(args.batchSize, nz, 1, 1)
+fixed_noise = torch.FloatTensor(args.batchSize, nz, 1, 1).normal_(0, 1)
+label = torch.FloatTensor(args.batchSize)
+real_label = 1
+fake_label = 0
+
+if args.cuda:
+    netD.cuda()
+    netG.cuda()
+    criterion.cuda()
+    input, label = input.cuda(), label.cuda()
+    noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+
+input = Variable(input)
+label = Variable(label)
+noise = Variable(noise)
+fixed_noise = Variable(fixed_noise)
+
+# setup optimizer
+optimizerD = optim.Adam(netD.parameters(), lr = args.lr, betas = (args.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr = args.lr, betas = (args.beta1, 0.999))
+
+for epoch in range(args.niter):
+    for i, data in enumerate(dataloader, 0):
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # train with real
+        netD.zero_grad()
+        real_cpu, _ = data
+        batch_size = real_cpu.size(0)
+        input.data.resize_(real_cpu.size()).copy_(real_cpu)
+        label.data.resize_(batch_size).fill_(real_label)
+
+        output = netD(input)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.data.mean()
+
+        # train with fake
+        noise.data.resize_(batch_size, nz, 1, 1)
+        noise.data.normal_(0, 1)
+        fake = netG(noise)
+        label.data.fill_(fake_label)
+        output = netD(fake.detach())
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.data.mean()
+        errD = errD_real + errD_fake
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.data.fill_(real_label) # fake labels are real for generator cost
+        output = netD(fake)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.data.mean()
+        optimizerG.step()
+
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+              % (epoch, args.niter, i, len(dataloader),
+                 errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+        if i % 100 == 0:
+            vutils.save_image(real_cpu,
+                    '%s/%s_real_samples.png' % (args.outf, args.name))
+            fake = netG(fixed_noise)
+            vutils.save_image(fake.data,
+                    '%s/%s_fake_samples_epoch_%03d.png' % (args.outf, args.name, epoch))
+
+    # do checkpointing
+    torch.save(netG.state_dict(), '%s/%s_netG_epoch_%d.pth' % (args.outf, args.name, epoch))
+    torch.save(netD.state_dict(), '%s/%s_netD_epoch_%d.pth' % (args.outf, args.name,vepoch))
